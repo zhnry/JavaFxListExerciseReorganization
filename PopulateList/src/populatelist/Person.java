d77b5d8c9425b298f57df3eec7557d877ba1d53d
Phil O'Connell <ist242spring2015@gmail.com>
2015-10-13 00:00:12 -0400

    Added @GeneratedValue so no complaints when we leave the id NULL.  The database will create the ID value.

--- PopulateList/src/populatelist/Person.java
+++ PopulateList/src/populatelist/Person.java
@@ -9,6 +9,8 @@ import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -28,6 +30,9 @@ import javax.persistence.Table;
 public class Person implements Serializable {
   private static final long serialVersionUID = 1L;
   @Id
+
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
+
   @Basic(optional = false)
   @Column(name = "id")
   private Integer id;
